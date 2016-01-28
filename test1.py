92fad62c79350390e5ae41fb78dca41a2fb345b8
Naresh Damji <Naresh Damji>
2016-01-28 17:57:27 +0530

    Added some code

--- test1.py
+++ test1.py
@@ -1,1 +1,8 @@
+def function1(arg1,arg2) :
+	print "%d + %d ", (arg1,arg2)
+	return (arg1+arg2)
+
+num1,num2=map(int,raw_input().split())
+print function1(num2,num1)
+
 print "testing of python code using git"
