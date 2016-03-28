ef096ad5407ea054a05144cec60abde7b2430422
Naresh <nareshdamji@gmail.com>
2016-03-28 19:24:35 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,6 +1,6 @@
 def function1(arg1,arg2) :
-	print "%d + %d ", (arg1,arg2)
-	return (arg1+arg2)
+	print "%d - %d ", (arg1,arg2)
+	return (arg1-arg2)
 
 num1,num2=map(int,raw_input().split())
 print function1(num2,num1)
